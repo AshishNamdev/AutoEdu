3a94982fa93186f6d13d209e523f695e9642b8d6
Ashish Namdev <ashish28.sirt@gmail.com>
2025-08-21 20:18:24 +0530

    Added backup_file function
    - - Added backup_file function in common.__init__.py to create timestamped backups in backup folder
    
    Signed-off-by: Ashish Namdev <ashish28.sirt@gmail.com>

--- common/__init__.py
+++ common/__init__.py
@@ -17,6 +17,8 @@ Functions:
     - login_user(): login user to the specified portal.
 """
 
+import os
+import shutil
 from datetime import datetime
 
 from common.driver import driver
@@ -57,3 +59,34 @@ def get_timestamp(format="%d-%m-%Y - %I:%M:%S%p"):
         if format
         else datetime.now().strftime("%Y%m%d_%H%M%S")
     )
+
+
+def backup_file(src_path, backup_dir="backup"):
+    """
+    Creates a timestamped backup of the given file in the specified backup directory.
+
+    Args:
+        src_path (str): Path to the source file to back up.
+        backup_dir (str): Directory where the backup will be stored. Defaults to 'backup'.
+
+    Returns:
+        str: Full path to the created backup file.
+
+    Raises:
+        IOError: If backup fails due to permission or disk issues.
+    """
+    if not os.path.isfile(src_path):
+        logger.debug(f"Source file not found: {src_path}")
+        return
+
+    os.makedirs(backup_dir, exist_ok=True)
+
+    base_name = os.path.basename(src_path)
+    name, ext = os.path.splitext(base_name)
+    timestamp = get_timestamp()  # Includes microseconds
+    backup_name = f"{name}_{timestamp}{ext}"
+    backup_path = os.path.join(backup_dir, backup_name)
+
+    logger.debug("%s --> %s", src_path, backup_path)
+    shutil.copy2(src_path, backup_path)  # Preserves metadata
+    return backup_path
